179cc828ab24a4826f194ac227704a699f95b198
Miguel <miguezaga2@hotmail.com>
2022-08-02 09:23:20 -0500

    added countdown functionality

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,25 +1,56 @@
-if (window.localStorage.getItem("releaseDate")) {
-  console.log("There is alreade a release Date");
-} else {
-  console.log("There is no release Date");
-  console.log("creating one...");
-  let newReleaseDate = createReleaseDate();
-  window.localStorage.setItem("releaseDate", newReleaseDate);
-}
+var countdown = document.getElementById("countdown");
 
-{
-  let releaseDate = window.localStorage.getItem("releaseDate");
-  let currentDate = new Date();
-
-  let releaseMiliseconds = Date.parse(releaseDate);
-  let currentMiliseconds = Date.parse(currentDate);
-
-  let milisecondsLeft = releaseMiliseconds - currentMiliseconds;
-  console.log(`release in ${Date.setTime(milisecondsLeft)}`);
-}
+createReleaseDate();
+renderReleaseDate();
+setInterval(renderCountdown, 1000);
 
 function createReleaseDate() {
-  var date = new Date();
-  date.setMonth(date.getMonth() + 1);
-  return date;
+  if (window.localStorage.getItem("releaseDate")) {
+    console.log("There is already a release Date");
+    console.log(window.localStorage.getItem("releaseDate"));
+  } else {
+    let date = new Date();
+    date.setTime(date.getTime() + 30 * 24 * 60 * 60 * 1000); // plus 30 days in miliseconds
+    window.localStorage.setItem("releaseDate", date);
+  }
+}
+
+function renderReleaseDate() {
+  var releaseDateElement = document.getElementById("release-date");
+  var releaseDate = new Date(window.localStorage.getItem("releaseDate"));
+  console.log(releaseDate);
+  var options = { day: "numeric", month: "long", year: "numeric" };
+  console.log(releaseDateElement);
+  releaseDateElement.innerHTML = releaseDate.toLocaleDateString(
+    "en-GB",
+    options
+  );
+}
+
+function renderCountdown() {
+  var countdownNumbers = countdown.getElementsByClassName("countdown__number");
+  var parsedCountdown = calculateCountdown();
+
+  console.log();
+
+  parsedCountdown.forEach(function renderNumbers(number, i) {
+    countdownNumbers[i].innerHTML = number;
+  });
+}
+
+function calculateCountdown() {
+  var releaseDate = new Date(window.localStorage.getItem("releaseDate"));
+  var currentDate = new Date();
+  var milisecondsLeft = releaseDate.getTime() - currentDate.getTime();
+
+  var conversionRates = [24 * 60 * 60 * 1000, 60 * 60 * 1000, 60 * 1000, 1000]; // miliseconds in [days, hours, minutes, seconds]
+
+  var parsedCountdown = conversionRates.map(function parseMiliseconds(rate) {
+    var convertedValue = Math.floor(milisecondsLeft / rate);
+    milisecondsLeft = milisecondsLeft % rate;
+    return convertedValue;
+  });
+
+  // [daysLeft, hoursLeft, minutesLeft, secondsLeft] = parsedCountdown;
+  return parsedCountdown;
 }
